9dff5634f691fc869cc5cf862722479c77601a43
pedropsoares <pedropaulos.1998@gmail.com>
2020-08-28 10:30:34 -0300

    CRUD methods implemented

--- app/Controllers/Http/TweetController.js
+++ app/Controllers/Http/TweetController.js
@@ -4,73 +4,49 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
-/**
- * Resourceful controller for interacting with tweets
- */
+const Tweet = use('App/Models/Tweet')
+
 class TweetController {
   /**
-   * Show a list of all tweets.
-   * GET tweets
-   *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-  }
+  async index () {
+    const tweets = Tweet.all()
 
-  /**
-   * Render a form to be used for creating a new tweet.
-   * GET tweets/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
+    console.log(tweets)
 
+    return tweets
+  }
   /**
-   * Create/save a new tweet.
-   * POST tweets
-   *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store ({ request, auth }) {
+    const data = request.only(['content'])
+    const tweet = await Tweet.create({
+      user_id: auth.user.id,
+      ...data
+    })
+
+    return tweet
   }
 
   /**
-   * Display a single tweet.
-   * GET tweets/:id
-   *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
   async show ({ params, request, response, view }) {
-  }
+    const tweet = await Tweet.findOrFail(params.id)
 
-  /**
-   * Render a form to update an existing tweet.
-   * GET tweets/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+    return tweet
   }
-
   /**
-   * Update tweet details.
-   * PUT or PATCH tweets/:id
-   *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
@@ -79,14 +55,18 @@ class TweetController {
   }
 
   /**
-   * Delete a tweet with id.
-   * DELETE tweets/:id
-   *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params, request, auth, response }) {
+    const tweet = await Tweet.findOrFail(params.id)
+
+    if (tweet.user_id !== auth.user.id) {
+      response.status(401)
+    }
+
+    await tweet.destroy()
   }
 }
 
--- start/routes.js
+++ start/routes.js
@@ -21,4 +21,8 @@ const Route = use('Route')
 Route.post('/register', 'AuthController.register')
 Route.post('/authenticate', 'AuthController.authenticate')
 
-Route.get('/app', 'AppController.index').middleware(["auth"])
+Route.group(() => {
+  Route.resource('tweets', 'TweetController')
+  .apiOnly()
+  .except('update')
+}).middleware('auth')
